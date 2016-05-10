ed8ac4b334fd19d760147ba71fa5745aed43bd55
MFigand <m.figand@gmail.com>
2016-05-10 18:12:49 +0200

    Update grid

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    f.email {Faker::Internet.email}
-    f.password '12345678'
-    f.password_confirmation '12345678'
-    f.name {Faker::Name.name}
-    f.avatar {Faker::Avatar.image}
-  end
-end
